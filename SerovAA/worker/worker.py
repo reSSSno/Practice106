cca7fe203d645b8772c814fb5f20295b90a0f648
reSSSno <46084217+reSSSno@users.noreply.github.com>
2026-05-23 19:43:32 +0300

    Update worker.py

--- SerovAA/worker/worker.py
+++ SerovAA/worker/worker.py
@@ -15,23 +15,22 @@ def generate_password(length, use_digits, use_special):
         chars += '!@#$%^&*()'
     return ''.join(random.choice(chars) for _ in range(length))
 
-while True:
-    task_id = redis_client.brpop('password_tasks')[1].decode()
-    
-    # Simulate long processing
-    time.sleep(2)
-    
-    task_data = json.loads(redis_client.get(f"task:{task_id}"))
-    try:
-        password = generate_password(
-            task_data['length'],
-            task_data['use_digits'],
-            task_data['use_special']
-        )
-        task_data['status'] = 'completed'
-        task_data['password'] = password
-    except Exception as e:
-        task_data['status'] = 'failed'
-        task_data['error'] = str(e)
-    
-    redis_client.set(f"task:{task_id}", json.dumps(task_data))
+# Этот код будет выполняться ТОЛЬКО при запуске worker.py как скрипта,
+# но НЕ при импорте из тестов
+if __name__ == "__main__":
+    while True:
+        task_id = redis_client.brpop('password_tasks')[1].decode()
+        time.sleep(2)  # имитация долгой обработки
+        task_data = json.loads(redis_client.get(f"task:{task_id}"))
+        try:
+            password = generate_password(
+                task_data['length'],
+                task_data['use_digits'],
+                task_data['use_special']
+            )
+            task_data['status'] = 'completed'
+            task_data['password'] = password
+        except Exception as e:
+            task_data['status'] = 'failed'
+            task_data['error'] = str(e)
+        redis_client.set(f"task:{task_id}", json.dumps(task_data))
